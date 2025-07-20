f1a580e805a78e7eec37276bddd0e3820a33633c
aya <ayahmida64@gmail.com>
2025-07-20 19:58:23 +0100

    hash password

--- DevAPI/Services/UserService.cs
+++ DevAPI/Services/UserService.cs
@@ -1,20 +1,26 @@
 ﻿using DevAPI.Entities;
 using DevAPI.Repositories;
+using Microsoft.AspNetCore.Identity;
 
 namespace DevAPI.Services
 {
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher<User> _passwordHasher; 
 
-        public UserService(IUserRepository userRepository)
+        public UserService(IUserRepository userRepository, IPasswordHasher<User> passwordHasher) 
         {
             _userRepository = userRepository;
+            _passwordHasher = passwordHasher; 
         }
 
         public async Task RegisterAsync(User user)
         {
             bool emailExists = await _userRepository.EmailExistsAsync(user.Email);
+
+            string hashedPassword = _passwordHasher.HashPassword(user, user.PasswordHash);
+            user.PasswordHash = hashedPassword;
 
             if (emailExists)
             {
@@ -23,7 +29,6 @@ namespace DevAPI.Services
 
             await _userRepository.AddUserAsync(user);
         }
-
-
     }
+
 }
